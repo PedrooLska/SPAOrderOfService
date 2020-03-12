bc28bb787a2c34d919deaeb3f2f2fb0b9b1c83aa
PedrooLska <pedrolucas81@hotmail.com>
2020-03-11 21:44:27 -0300

    Create Header

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -4,7 +4,43 @@ import "./index.css";
 const Header = () => {
   return (
     <header className="header">
-      <h1>Title</h1>
+      <div className="header__logo">
+        <a href="#">
+          <h2>My App</h2>
+        </a>
+      </div>
+
+      <div className="header__nav">
+        <ul>
+          <li>
+            <a href="#">Página Principal</a>
+          </li>
+
+          <li>
+            <a href="#">Abrir OS</a>
+          </li>
+
+          <li>
+            <a href="#">Consultar OS</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="header__socialmedia">
+        <ul>
+          <li>
+            <a href="#">Facebook</a>
+          </li>
+
+          <li>
+            <a href="#">Instagram</a>
+          </li>
+
+          <li>
+            <a href="#">Linkedin</a>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
