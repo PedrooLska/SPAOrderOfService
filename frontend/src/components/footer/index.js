20816e189d111ca609edc55d19a2dc0bb5f2cf45
PedrooLska <pedrolucas81@hotmail.com>
2020-03-11 22:05:49 -0300

    Create Footer

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./index.css";
 
+import { IoMdHeart } from "react-icons/io";
+
 const Footer = () => {
   return (
     <footer className="footer">
-      <h1>Footer</h1>
+      <h3>
+        Pedro Lucas <IoMdHeart color="red" size="20px" />
+      </h3>
     </footer>
   );
 };
